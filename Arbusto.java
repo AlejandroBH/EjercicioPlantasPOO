464a7e31a424bc13377bee96c508ae5bc4a571b0
Alejandro Barrera <alejandro91dark@hotmail.es>
2022-07-15 20:21:36 -0400

    Se agregaron GetSet y metodos Constructores a clases hijas

--- Arbusto.java
+++ Arbusto.java
@@ -8,8 +8,60 @@ public class Arbusto extends Planta {
     private String colorHojas;
     private boolean sePodaONo;
 
+    public Arbusto() {
+    }
+
+    public Arbusto(double anchoArbusto, boolean esDomestico, int variedadArbusto, String colorHojas, boolean sePodaONo, String nombre, double altoTallo, boolean tieneHojas, String clima) {
+        super(nombre, altoTallo, tieneHojas, clima);
+        this.anchoArbusto = anchoArbusto;
+        this.esDomestico = esDomestico;
+        this.variedadArbusto = variedadArbusto;
+        this.colorHojas = colorHojas;
+        this.sePodaONo = sePodaONo;
+    }
+
+    public double getAnchoArbusto() {
+        return anchoArbusto;
+    }
+
+    public void setAnchoArbusto(double anchoArbusto) {
+        this.anchoArbusto = anchoArbusto;
+    }
+
+    public boolean isEsDomestico() {
+        return esDomestico;
+    }
+
+    public void setEsDomestico(boolean esDomestico) {
+        this.esDomestico = esDomestico;
+    }
+
+    public int getVariedadArbusto() {
+        return variedadArbusto;
+    }
+
+    public void setVariedadArbusto(int variedadArbusto) {
+        this.variedadArbusto = variedadArbusto;
+    }
+
+    public String getColorHojas() {
+        return colorHojas;
+    }
+
+    public void setColorHojas(String colorHojas) {
+        this.colorHojas = colorHojas;
+    }
+
+    public boolean isSePodaONo() {
+        return sePodaONo;
+    }
+
+    public void setSePodaONo(boolean sePodaONo) {
+        this.sePodaONo = sePodaONo;
+    }
+
     @Override
-    protected void mostrarMensaje() {
+    public void mostrarMensaje() {
         System.out.println("Hola soy un arbusto");
     }
     
--- Flor.java
+++ Flor.java
@@ -8,8 +8,60 @@ public class Flor extends Planta {
     private int variedadFlor;
     private String estacion;
 
+    public Flor() {
+    }
+
+    public Flor(String colorPetalos, int cantPetalos, String colorPistilo, int variedadFlor, String estacion, String nombre, double altoTallo, boolean tieneHojas, String clima) {
+        super(nombre, altoTallo, tieneHojas, clima);
+        this.colorPetalos = colorPetalos;
+        this.cantPetalos = cantPetalos;
+        this.colorPistilo = colorPistilo;
+        this.variedadFlor = variedadFlor;
+        this.estacion = estacion;
+    }
+
+    public String getColorPetalos() {
+        return colorPetalos;
+    }
+
+    public void setColorPetalos(String colorPetalos) {
+        this.colorPetalos = colorPetalos;
+    }
+
+    public int getCantPetalos() {
+        return cantPetalos;
+    }
+
+    public void setCantPetalos(int cantPetalos) {
+        this.cantPetalos = cantPetalos;
+    }
+
+    public String getColorPistilo() {
+        return colorPistilo;
+    }
+
+    public void setColorPistilo(String colorPistilo) {
+        this.colorPistilo = colorPistilo;
+    }
+
+    public int getVariedadFlor() {
+        return variedadFlor;
+    }
+
+    public void setVariedadFlor(int variedadFlor) {
+        this.variedadFlor = variedadFlor;
+    }
+
+    public String getEstacion() {
+        return estacion;
+    }
+
+    public void setEstacion(String estacion) {
+        this.estacion = estacion;
+    }
+
     @Override
-    protected void mostrarMensaje() {
+    public void mostrarMensaje() {
         System.out.println("Hola soy una flor");
     }
     
